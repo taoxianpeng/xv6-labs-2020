8c960218fcb1b56a6c30ad70ff71622d511925dd
Xianpeng Tao <492350627@qq.com>
2022-12-05 21:30:09 +0800

    add: 完成打印页表

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,27 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+
+void _vmprint(pagetable_t pagetable, int level){
+  for(int i=0; i<512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      uint64 child = PTE2PA(pte);
+      for(int j=0; j<level; ++j){
+        if(j) printf(" ");
+        printf("..");
+      }
+      printf("%d: pte %p pa %p\n",i, pte, child);
+      // 查看flag位是否被设置，若被设置则为最低一层，
+      // 见vm.c161行，可以看到只有最底层被设置了符号位
+      if((pte & (PTE_R|PTE_W|PTE_X)) == 0)
+        _vmprint((pagetable_t)child, level+1);
+    }
+  }
+}
+
+void vmprint(pagetable_t pagetable){
+  printf("page table %p\n", pagetable);
+  _vmprint(pagetable, 1);
+}
